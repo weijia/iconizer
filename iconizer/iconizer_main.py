f9f941f535f4d4e65e845534f9981d7f2680bda7
Richard <richardwangwang@gmail.com>
2013-09-03 23:05:34 +0800

    Added execute function.

--- iconizer/iconizer_main.py
+++ iconizer/iconizer_main.py
@@ -9,6 +9,7 @@ from iconizer.qtconsole.PyQtGuiFactory import PyQtGuiFactory
 class Iconizer(threading.Thread):
     def __init__(self):
         super(Iconizer, self).__init__()
+        self.launched_app_dict = {}
 
     def start_gui(self):
         self.launcher = CrossGuiLauncher(PyQtGuiFactory())
@@ -33,12 +34,22 @@ class Iconizer(threading.Thread):
     '''
 
     def execute(self, app_descriptor_dict):
+        """
+        Launch app in iconized mode
+        :param app_descriptor_dict: example: {"testapp_id_for_later_killing": ["d:/testapp.bat"]}
+        :return: N/A
+        """
         #Send request to start a new app
-        self.server.launch(app_descriptor_dict)
+        for key in app_descriptor_dict:
+            self.launched_app_dict[key] = {
+                "collector": self.launcher.create_console_wnd_for_app(app_descriptor_dict[key]),
+                "params": app_descriptor_dict[key],
+            }
 
 
 def main():
     Iconizer().start_gui()
+
 
 if __name__ == '__main__':
     main()

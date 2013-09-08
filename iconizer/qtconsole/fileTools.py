d87a040c87c44512a1e583aeae46eee8f124d5bb
Richard <richardwangwang@gmail.com>
2013-09-08 16:19:46 +0800

    Created work around for cx freeze don't have distutils.dist issue.

--- iconizer/qtconsole/fileTools.py
+++ iconizer/qtconsole/fileTools.py
@@ -1,19 +1,24 @@
-#import os
-from pkg_resources import Requirement, resource_filename, resource_exists
+try:
+    from pkg_resources import Requirement, resource_filename, resource_exists
+
+    def find_resource_in_pkg(filename):
+        generated_path = resource_filename(Requirement.parse("iconizer"), "iconizer/qtconsole/%s" % filename)
+        print generated_path
+        if resource_exists(Requirement.parse("iconizer"), "iconizer/qtconsole/%s" % filename):
+            return resource_filename(Requirement.parse("iconizer"), "iconizer/qtconsole/%s" % filename)
+        print filename, "not found"
+        raise "file not found"
+        return None
+except:
+    import os
+
+    def find_resource_in_pkg(filename):
+        p = os.getcwd()
+        for dirpath, dirnames, filenames in os.walk(p):
+            if filename in filenames:
+                #print 'find file:', os.path.join(dirpath, filename)
+                return os.path.join(dirpath, filename)
 
 
-def find_resource_in_pkg(filename):
-    '''
-    p = os.getcwd()
-    for dirpath, dirnames, filenames in os.walk(p):
-        if filename in filenames:
-            #print 'find file:', os.path.join(dirpath, filename)
-            return os.path.join(dirpath, filename)
-    '''
-    generated_path = resource_filename(Requirement.parse("iconizer"), "iconizer/qtconsole/%s" % filename)
-    print generated_path
-    if resource_exists(Requirement.parse("iconizer"), "iconizer/qtconsole/%s" % filename):
-        return resource_filename(Requirement.parse("iconizer"), "iconizer/qtconsole/%s" % filename)
-    print filename, "not found"
-    raise "file not found"
-    return None
+
+

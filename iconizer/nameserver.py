4418f16dc25614e9853163bbff789e54b7651499
Richard <richardwangwang@gmail.com>
2014-02-11 13:55:56 +0800

    Fixing name service not quit issue.

--- iconizer/nameserver.py
+++ iconizer/nameserver.py
@@ -7,20 +7,31 @@ class NameServerInThread(threading.Thread):
         super(NameServerInThread, self).__init__()
         self.name_server_daemon = None
 
-    def run(self):
-        nsUri, daemon, bcserver = Pyro4.naming.startNS()
-        self.name_server_daemon = daemon
-        print nsUri, daemon, bcserver
+    @staticmethod
+    def is_name_server_started():
         try:
-            daemon.requestLoop()
+            ns = Pyro4.locateNS()
+            return True
         except:
-            import traceback
-            traceback.print_exc()
-        finally:
-            daemon.close()
-            if bcserver is not None:
-                bcserver.close()
-        print("NS shut down.")
+            return False
+
+    def run(self):
+        if self.is_name_server_started():
+            return
+        else:
+            nsUri, daemon, bcserver = Pyro4.naming.startNS()
+            self.name_server_daemon = daemon
+            print nsUri, daemon, bcserver
+            try:
+                daemon.requestLoop()
+            except:
+                import traceback
+                traceback.print_exc()
+            finally:
+                daemon.close()
+                if bcserver is not None:
+                    bcserver.close()
+            print("NS shut down.")
 
     def shutdown(self):
         self.name_server_daemon.shutdown()

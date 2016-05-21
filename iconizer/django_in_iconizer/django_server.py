c3d03629734abfead5ae1eae83d1b6dcec792b45
Richard <richardwangwang@gmail.com>
2016-05-21 14:41:23 +0800

    Enable specify command line processor.

--- iconizer/django_in_iconizer/django_server.py
+++ iconizer/django_in_iconizer/django_server.py
@@ -10,6 +10,7 @@ class DjangoServer(object):
             self.django_manage_script = self.default_django_manage_script
         else:
             self.django_manage_script = django_manage_script
+        self.django_manage_script = os.environ.get("MANAGE_PY", self.django_manage_script)
 
     def get_task_descriptor(self, task_name, param_list=[]):
         task_name_and_param = [self.django_manage_script, task_name]

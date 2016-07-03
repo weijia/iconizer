98f0fef1f9d6e867f6a110945aea3d01419101c3
Richard <richardwangwang@gmail.com>
2016-07-03 19:10:56 +0800

    Fix manage.exe full path missing issue.

--- iconizer/django_in_iconizer/django_server.py
+++ iconizer/django_in_iconizer/django_server.py
@@ -4,6 +4,9 @@ from ufs_tools.app_framework import get_executable_folder
 
 
 class DjangoServerBase(object):
+    """
+    Full path must be provided, so the executed app can have the correct folder info
+    """
     def __init__(self, django_manage_script=None):
         super(DjangoServerBase, self).__init__()
         if django_manage_script is None:
@@ -11,15 +14,19 @@ class DjangoServerBase(object):
         else:
             self.django_manage_script = django_manage_script
         self.django_manage_script = os.environ.get("MANAGE_PY", self.django_manage_script)
+        self.exe_folder = get_executable_folder()
+
+    def get_full_path(self, filename):
+        return os.path.join(self.exe_folder, filename)
 
     def get_task_descriptor(self, task_name, param_list=None):
         param_list = param_list or []
-        task_name_and_param = [self.django_manage_script, task_name]
+        task_name_and_param = [self.get_full_path(self.django_manage_script), task_name]
         task_name_and_param.extend(param_list)
         return {task_name: task_name_and_param}
 
     def execute_cmd(self, django_cmd):
-        print "!!!!!!!!!!!!!!! cur folder:", os.getcwd()
+        # print "!!!!!!!!!!!!!!! cur folder:", os.getcwd()
         os.system(self.get_cmd_str(django_cmd))
 
 
@@ -40,13 +47,14 @@ class DjangoServerExe(DjangoServerBase):
     # noinspection PyMethodMayBeStatic
     def get_cmd_str(self, cmd_name, param_list=[]):
         exe_folder = get_executable_folder()
-        return "%s %s" % (os.path.join(exe_folder, self.django_manage_script), cmd_name)
+        print "exe_folder in django_server:", exe_folder
+        return "%s %s" % (self.get_full_path(self.django_manage_script), cmd_name)
 
     # noinspection PyMethodMayBeStatic
     def get_run_server_task_descriptor(self, params=None):
         params = params or []
         exe_folder = get_executable_folder()
-        cmd_and_param = [os.path.join(exe_folder, "cherrypy_server.exe")]
+        cmd_and_param = [self.get_full_path("cherrypy_server.exe")]
         cmd_and_param.extend(params)
         # print "pwd:", os.getcwd()
         # print "!!!!!!!!!!!!!!!!!!!!!!!!!", exe_folder

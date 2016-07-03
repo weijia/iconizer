7f35f1c43198e9db90738c94bc392a2cf7d541c8
Richard <richardwangwang@gmail.com>
2016-07-03 16:29:36 +0800

    Fix runserver issue by use cherrypy.

--- iconizer/django_in_iconizer/django_server.py
+++ iconizer/django_in_iconizer/django_server.py
@@ -1,5 +1,7 @@
 import os
 
+from ufs_tools.app_framework import get_executable_folder
+
 
 class DjangoServerBase(object):
     def __init__(self, django_manage_script=None):
@@ -11,7 +13,7 @@ class DjangoServerBase(object):
         self.django_manage_script = os.environ.get("MANAGE_PY", self.django_manage_script)
 
     def get_task_descriptor(self, task_name, param_list=None):
-        param_list = param_list or None
+        param_list = param_list or []
         task_name_and_param = [self.django_manage_script, task_name]
         task_name_and_param.extend(param_list)
         return {task_name: task_name_and_param}
@@ -27,6 +29,9 @@ class DjangoServer(DjangoServerBase):
     def get_cmd_str(self, cmd_name, param_list=[]):
         return "python %s %s" % (self.django_manage_script, cmd_name)
 
+    def get_run_server_task_descriptor(self, params=None):
+        return self.get_task_descriptor("runserver", ["0.0.0.0:8110"])
+
 
 class DjangoServerExe(DjangoServerBase):
     default_django_manage_script = "manage.exe"
@@ -34,3 +39,14 @@ class DjangoServerExe(DjangoServerBase):
     # noinspection PyMethodMayBeStatic
     def get_cmd_str(self, cmd_name, param_list=[]):
         return "%s %s" % (self.django_manage_script, cmd_name)
+
+    # noinspection PyMethodMayBeStatic
+    def get_run_server_task_descriptor(self, params=None):
+        params = params or []
+        exe_folder = get_executable_folder()
+        cmd_and_param = [os.path.join(exe_folder, "cherrypy_server.exe")]
+        cmd_and_param.extend(params)
+        # print "pwd:", os.getcwd()
+        # print "!!!!!!!!!!!!!!!!!!!!!!!!!", exe_folder
+        # os.chdir(exe_folder)
+        return {"run server": cmd_and_param}
